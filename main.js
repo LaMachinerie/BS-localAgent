966a46eff95024c31c125360c6e8e0683a9f3e4b
JulesTopart <jules.topart@gmail.com>
2019-07-18 15:43:30 +0200

    hex

--- main.js
+++ main.js
@@ -122,7 +122,8 @@ Builder.compile = function (res) {
     console.log(err)
     var hex = undefined; 
     try {
-        hex = fs.readFileSync(basePath + + '/build/sketch.ino.hex');
+        hex = fs.readFileSync(basepath + + '/build/sketch.ino.hex');
+        console.log(hex);
     } catch (error) {
         err = error;
     }
