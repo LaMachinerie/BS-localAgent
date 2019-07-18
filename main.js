ce38676c5f88eba080cb20cd0d826da3cb7d5faa
JulesTopart <jules.topart@gmail.com>
2019-07-18 14:21:21 +0200

    compiler

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 //Raspizza JS remote Arduino uploader
-const fs            = require('fs');
+
+//Web interaction library
 const express       = require('express')
-var   cors          = require('cors')
-const portscanner   = require('portscanner')
-const bodyParser    = require("body-parser");
-const avrgirl       = require("lite-avrgirl-lillypad")
-const app           = express()
+const bodyParser    = require('body-parser')
+const app           = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+
 var Blink = "void setup() {pinMode(13, OUTPUT);}void loop() {digitalWrite(13, HIGH);delay(1000);digitalWrite(13, LOW);delay(1000);}"
 
 page = '<!DOCTYPE html>' +
@@ -47,45 +45,26 @@ pageEnd = '</p>' +
     '      <form>' +
     '       <input name="code" type="hidden" value="' + Blink + '">' +
     '       <input name="type" type="hidden" value="upload">' +    
-    '       <button type="submit" formmethod="post">Upload Blink.ino</button>' +
+    '       <button type="submit" formaction="/upload" formmethod="post">Upload Blink.ino</button>' +
     '      </form>' +
     '    </div>' +
     '  </body>' +
     '</html>';
 
-portscanner.findAPortNotInUse(3000, 3000, '127.0.0.1', function (error, port) {
-    if (error) {
-        app.get('/', (req, res) => res.send(error))
-    }
-    console.log('AVAILABLE PORT AT: ' + port)
-    page += port + pageEnd;
-    app.listen(port, () => console.log(`Agent listening on port ${port}!`))
-});
-
-
+var port = 3000;
+page += port + pageEnd;
+app.listen(port);
 
 
 app.get('/', (req, res) => res.send(page))
 
 app.post('/', function (req, res) {
-    var type = req.body.type;
+    res.end("OK");
+});
+
+app.post('/upload', function (req, res) {
     var code= req.body.code;
-    if(type == "check"){
-        res.end("OK");
-        return;
-    }else if(type == "upload"){
-        console.log(type);
-        console.log(code);
-        res.end("Uploading...");
-        return;
-    }else if(type == "ide"){
-        console.log(type);
-        console.log(code);
-        res.end("Opening...");
-        return;
-    }else{
-        res.end("Unknown request")
-        console.log(req.body);
-        return;
-    }
+
+    console.log(code);
+    res.end("compiling : \n" + code);
 });
