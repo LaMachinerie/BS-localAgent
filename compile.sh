311be8a1cda575c58c03984728bc367a668c4dd3
JulesTopart <jules.topart@gmail.com>
2019-07-18 17:21:34 +0200

    avr due !

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ arduino-builder \
 -verbose=false \
 -hardware /usr/share/arduino/hardware \
 -build-path=/opt/BS-localAgent/build \
--tools /usr/share/arduino/hardware/tools \
--tools /opt/BS-localAgent/builder/tools-builder \
+-tools /usr/share/arduino/hardware/tools/avr \
+-tools /opt/BS-localAgent/build/tools-builder \
 -libraries=/opt/BS-localAgent/builder/libraries \
 -fqbn=arduino:avr:LilyPadUSB /opt/BS-localAgent/sketch/sketch.ino
