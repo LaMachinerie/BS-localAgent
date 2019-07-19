4cf74b730a7032603fc5c19ac57c194074412900
JulesTopart <jules.topart@gmail.com>
2019-07-19 09:37:26 +0200

    scirpt update

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
-./builder/arduino-builder \
--compile \
--hardware /usr/share/arduino/hardware \
--tools /usr/share/arduino/hardware/tools/avr \
--tools /opt/BS-localAgent/builder/ \
--build-path=/opt/BS-localAgent/build \
--tools /opt/BS-localAgent/builder/tools \
--libraries=/opt/BS-localAgent/builder/libraries \
--fqbn=arduino:avr:LilyPadUSB /opt/BS-localAgent/sketch/sketch.ino
+#!/bin/bash
+
+SCREEN=3
+
+Xvfb :$SCREEN -nolisten tcp -screen :$SCREEN 1280x800x24 &
+
+xvfb="$!"
+
+DISPLAY=:$SCREEN arduino $@
+
+kill -9 $xvfb
